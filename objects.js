b680adf043e43ce019a1f4da11a868fade0b9d41
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-06-19 01:09:11 -0700

    complete objects topics

---
+++ objects.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  console.log(a + b);
+}
+
+let newObject = {
+  name: "John Doe",
+  age: 25,
+  func: (a, b) => {
+    console.log(a + b);
+  },
+};
+
+console.log(newObject.name);
+console.log(newObject.age);
+newObject.func(2, 3);
+
+
